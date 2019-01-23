974261522d3705d2736bcdddcd61ea4b533ed3c2
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2019-01-23 07:31:54 -0700

    Adding example configurations.

--- old/labcas-biomarker-discovery/scripts/ecs/run_stock_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Script that runs 1 iteration of the biomarker discovery stock pipeline.
-# The iteration number is provided as input argument.
-# Output is written to $LABCAS_STAGING
-
-cd $LABCAS_STAGING
-R --no-save < $LABCAS_HOME/workflows/biomarker-discovery/pges/run_stock_pipeline_iter.R --args $1
--- old/labcas-biomarker-discovery/scripts/start_filemgr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# script to start the file manager through supervisor in non-daemon mode
-
-export SOLR_INSTALL_DIR=${LABCAS_HOME}/solr
-export SOLR_HOME=${LABCAS_HOME}/solr-home
-export SOLR_DATA_DIR=${LABCAS_HOME}/solr-index
-/usr/local/bin/supervisord -c /etc/supervisor/supervisord-filemgr.conf
